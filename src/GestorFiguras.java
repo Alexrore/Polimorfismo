2e2a3fdcea4e9a3e7edefac094fb1150441d180f
erik medialdea arribas <erikmedialdea@gmail.com>
2025-09-16 11:33:17 +0200

    Funciona todo.

--- src/GestorFiguras.java
+++ src/GestorFiguras.java
@@ -2,41 +2,40 @@ import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class GestorFiguras {
 
     public GestorFiguras() {
 
     }
-    public static double sum(Shape[] v) //Sirve para sumar las areas
-    {
-        double total = 0;
-        for (Shape s : v) //Recorre el vector
+        public static double sum (Shape[]v) //Sirve para sumar las areas
         {
-            total += s.area();
+            double total = 0;
+            for (Shape s : v) //Recorre el vector
+            {
+                total += s.area();
+            }
+            return total;
         }
-        return total;
-    }
-    public static void sort(Shape[] v) //Usa el comprareTo para ordenarlos
-    {
-        Arrays.sort(v);
-    }
+        public static void sort (Shape[]v) //Usa el comprareTo para ordenarlos
+        {
+            Arrays.sort(v);
+        }
 
-    public static void print(Shape[] v) {
-        for (Shape s : v)
-        {
-            System.out.println(s);
+        public static void print (Shape[]v){
+            for (Shape s : v) {
+                System.out.println(s);
+            }
         }
-    }
 
     public static void main(String[] args) {
-        Shape[] v = new Shape[4];
+        Shape[] v = new Shape[5];
 
 
         v[0] = new Rectangle(5, 3);
         v[1] = new Circle(5);
         v[2] = new Square(5);
-        v[3] = new Circle(120);
-        v[4] = new TriangleEQ(4, 2);
+        v[3] = new Circle(7);
+        v[4] = new TriangleEQ(6, 7);
 
         double res = GestorFiguras.sum(v);
 
--- src/TriangleEQ.java
+++ src/TriangleEQ.java
@@ -1,20 +1,16 @@
 public class TriangleEQ extends Shape{
 
     //Atributos
-    public class TriangleEQ extends Shape {
+    public double b;
+    public double h;
 
-        //Atributos
-        public double b;
-        public double h;
-
-        //Constructor
-        public TriangleEQ(double b, double h) {
-            this.b = b;
-            this.h = h;
-        }
-        //Metodos
-        public double area() {
-            return b*h/2;
-        }
+    //Constructor
+    public TriangleEQ(double b, double h) {
+        this.b = b;
+        this.h = h;
+    }
+    //Metodos
+    public double area() {
+        return b*h/2;
     }
 }
